7e97f38243bc9b69d6ba6a5cd2072e4d7b50b472
CharlieHVu <char1997@vt.edu>
2017-11-01 15:24:52 -0400

    Changed files to contain 8M datasets and wrote Matlab script to parse data

--- DataConvert.m
+++ DataConvert.m
@@ -1,5 +1,18 @@
-BPSK = read_complex_binary('.signals/BPSK_SNR_20');
-QPSK = read_complex_binary('.signals/QPSK_SNR_20');
-QAM16 = read_complex_binary('.signals/QAM16_SNR_20');
-QAM64 = read_complex_binary('.signals/QAM64_SNR_20');
-VT = read_complex_binary('.signals/VT_SNR_20');
+BPSK = read_complex_binary('BPSK_SNR_20.dat');
+QPSK = read_complex_binary('QPSK_SNR_20.dat');
+QAM16 = read_complex_binary('QAM16_SNR_20.dat');
+QAM64 = read_complex_binary('QAM64_SNR_20.dat');
+VT = read_complex_binary('VT_SNR_20.dat');
+
+mod = QPSK;
+new = 2:length(mod);
+
+for i = 1:length(mod)
+    currSignal = mod(i, 1);
+    new(1, i) = real(currSignal);
+    new(2, i) = imag(currSignal);
+end
+    
+
+
+
--- dataset/DataConvert.m
+++ dataset/DataConvert.m
@@ -4,4 +4,15 @@ QAM16 = read_complex_binary('QAM16_SNR_20.dat');
 QAM64 = read_complex_binary('QAM64_SNR_20.dat');
 VT = read_complex_binary('VT_SNR_20.dat');
 
+mod = QPSK;
+new = 2:length(mod);
+
+for i = 1:length(mod)
+    currSignal = mod(i, 1);
+    new(1, i) = real(currSignal);
+    new(2, i) = imag(currSignal);
+end
+    
+
+
 
