2dabec06417b3a5f1876ae6d37fbab23d38722f4
CharlieHVu <char1997@vt.edu>
2017-11-01 15:04:46 -0400

    Changed files to have 8M datasets each for easier parsing

---
+++ DataConvert.m
@@ -0,0 +1,5 @@
+BPSK = read_complex_binary('.signals/BPSK_SNR_20');
+QPSK = read_complex_binary('.signals/QPSK_SNR_20');
+QAM16 = read_complex_binary('.signals/QAM16_SNR_20');
+QAM64 = read_complex_binary('.signals/QAM64_SNR_20');
+VT = read_complex_binary('.signals/VT_SNR_20');
---
+++ dataset/DataConvert.m
@@ -0,0 +1,7 @@
+BPSK = read_complex_binary('BPSK_SNR_20.dat');
+QPSK = read_complex_binary('QPSK_SNR_20.dat');
+QAM16 = read_complex_binary('QAM16_SNR_20.dat');
+QAM64 = read_complex_binary('QAM64_SNR_20.dat');
+VT = read_complex_binary('VT_SNR_20.dat');
+
+
