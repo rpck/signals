939f0707f11eebbfed1432aa0aa8c1522d8ffc9a
Kenneth Worden <kenny.worden@gmail.com>
2017-11-13 13:47:40 -0500

    Get network training, albeit poorly (~20%)

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from keras.utils import np_utils
 from keras import backend as K
 import re
 
-batch_size = 32
+num_categories = 5
+batch_size = 64
 epochs = 5
 
 def label_to_int(label):
@@ -50,18 +51,26 @@ def load_dataset(dataset_dir):
                 dataset.append(data)
                 labels.append(label_int)
             i += 1
-    return (np.array(dataset), np.array(labels)), (np.array(test), np.array(test_labels))
+    np_dataset = np.array(dataset)
+    np_labels = np.array(labels)
+    # Shuffle the dataset and labels with the same permutation
+    perm = np.random.permutation(np_dataset.shape[0])
+    shuffled_dataset = np_dataset[perm]
+    shuffled_labels = np_labels[perm]
+    return (shuffled_dataset, shuffled_labels), (np.array(test), np.array(test_labels))
 
 train_set, test_set = load_dataset('dataset')
+print(train_set[1])
+
 train_dataset = train_set[0].reshape(len(train_set[0]), 1, 512, 2)
 test_dataset = test_set[0].reshape(len(test_set[0]), 1, 512, 2)
 input_shape = (1, 512, 2)
-train_labels = np_utils.to_categorical(train_set[1], 5)
-test_labels = np_utils.to_categorical(test_set[1], 5)
+train_labels = np_utils.to_categorical(train_set[1], num_categories)
+test_labels = np_utils.to_categorical(test_set[1], num_categories)
 
 # Use tanh instead of ReLU to prevent NaN errors
 model = Sequential()
-model.add(Conv2D(32,
+model.add(Conv2D(16,
         kernel_size=(6, 2),
         activation='tanh',
         padding='same',
@@ -71,8 +80,8 @@ model.add(MaxPooling2D(pool_size=(2, 2),
         padding='same',
         data_format=None))
 model.add(Dropout(0.25))
-model.add(Conv2D(32,
-        kernel_size=(2, 2),
+model.add(Conv2D(16,
+        kernel_size=(7, 2),
         activation='tanh',
         padding='same'))
 model.add(MaxPooling2D(pool_size=(2, 2),
@@ -82,12 +91,12 @@ model.add(MaxPooling2D(pool_size=(2, 2),
 model.add(Flatten())
 
 #"Squash" to probabilities
-model.add(Dense(5, activation='softmax'))
+model.add(Dense(num_categories, activation='softmax'))
 
 model.summary()
 
 # Use a Stochastic-Gradient-Descent as a learning optimizer
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+sgd = SGD(lr=5, decay=1e-6, momentum=0.9, nesterov=True)
 
 # Prevent kernel biases from being exactly 0 and giving nan errors
 def constrainedCrossEntropy(ytrue, ypred):
